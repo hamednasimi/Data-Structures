32f2e6d6f014f35799fb06b76121803fce490f7e
hamed <hamednasimi98@gmail.com>
2023-07-21 16:05:01 +0330

    destructor added

--- Array/Array.h
+++ Array/Array.h
@@ -1,15 +1,19 @@
 #pragma once
 
-template <typename T, int S>
+template <typename T>
 class Array
 {
 private:
-	T* m_values = new T[S];
+	T* m_values;
 public:
 	template <typename... Args>
-	Array(Args... args) : m_values{ args... } {};
+	Array(int size, Args... args)
+	{
+		this->m_values = new T[size]{args...};
+	}
+
 	~Array()
-	{ 
+	{
 		delete[] m_values;
 	}
 
