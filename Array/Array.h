2e4379edeafdaabd92f55077f894d2e50cabbf71
hamednasimi <hamednasimi98@gmail.com>
2023-07-22 18:23:24 +0330

    Added a few methods, made the Array resizeable and ND compatible. updated README

--- Array/Array.h
+++ Array/Array.h
@@ -1,29 +1,42 @@
 #pragma once
 #include <iostream>
 
-template <typename T, int ... S>
+template <typename T>
 class Array
 {
 private:
 	int m_size{ 0 };
 	T* m_values{ 0 };
+	int m_sum{ 0 };
+	const char* m_type;
 public:
+	// Default constructor
+	Array()
+	{
+		this->m_values = nullptr;
+		this->m_type = typeid(*this).name();
+	}
+
 	// Runtime compatible
 	Array(int size)
 	{
 		this->m_size = size;
 		this->m_values = new T[m_size]{};
+		this->m_type = typeid(*this).name();
+		//std::cout << m_type;
 	}
 
 	// Only used when the size is known at compile time and the values are passed in through an array
 	template <typename... Args>
 	Array(Args... args)
 	{
-		this->m_size = { S... };
+		this->m_size = sizeof...(args);
 		if (m_size > 0)
 			this->m_values = new T[m_size]{ args... };
 		else
 			this->m_values = new T[0]{};
+		this->m_type = typeid(*this).name();
+		//std::cout << m_type;
 	}
 
 	~Array()
@@ -31,10 +44,66 @@ public:
 		delete[] m_values;
 	}
 
+	void resize(int new_size)
+	{
+		if (m_values != nullptr)
+		{
+			delete[] m_values;
+		}
+		this->m_size = new_size;
+		this->m_values = new T[m_size]{};
+	}
+
 	T& operator[](unsigned const int index)
 	{
 		return m_values[index];
 	}
 
-	int length() { return m_size; };
+	void represent()
+	{
+		std::cout << m_size << std::endl;
+		std::cout << m_type << std::endl;
+	}
+
+	int length()
+	{
+		return m_size;
+	}
+
+	const char* type()
+	{
+		return m_type;
+	}
+
+	template<typename O>
+	void populate(O obj)
+	{
+		for (int i{ 0 }; i < m_size; i++)
+		{
+			this->m_values[i] = obj;
+		}
+	}
+
+	// Numerical only
+	int sum()
+	{
+		int current_sum{ 0 };
+		for (int i{ 0 }; i < m_size; i++)
+		{
+			current_sum += m_values[i];
+		}
+		this->m_sum = current_sum;
+		return m_sum;
+	}
+
+	int sum_all()
+	{
+		int current_sum{ 0 };
+		for (int i{ 0 }; i < m_size; i++)
+		{
+			current_sum += m_values[i].sum();
+		}
+		this->m_sum = current_sum;
+		return m_sum;
+	}
 };
--- Array/README.md
+++ Array/README.md
@@ -13,7 +13,27 @@ Usage:
 		std::cout << runtimeArray[i] << std::endl;
 
 	// Compile time usage
-	Array<int, 2> compileTimeArray{ 7, 6 };
+	Array<int> compileTimeArray{ 7, 6 };
 	for (int i{ 0 }; i < compileTimeArray.length(); i++)
 	std::cout << compileTimeArray[i] << std::endl;
+
+	// ND arrays
+	Array<Array<int> > arr(6);
+
+	for (int i{ 0 }; i < arr.length(); i++)
+	{
+		arr[i].resize(6);
+		arr[i].populate(0);
+	}
+
+	for (int i{ 0 }; i < arr.length(); i++)
+	{
+		for (int j{ 0 }; j < arr[i].length(); j++)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	// Works with 2D arrays only for now
+	std::cout << arr.sum_all() << " ";
 ```
