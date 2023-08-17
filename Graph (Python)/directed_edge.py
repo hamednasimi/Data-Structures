45413f4b9af02eb7dfa1362741c654ef07ded706
hamednasimi <hamednasimi98@gmail.com>
2023-08-17 17:59:51 +0330

    Minor formatting issue fixes on directed graph.

--- Graph (Python)/directed_edge.py
+++ Graph (Python)/directed_edge.py
@@ -1,12 +1,13 @@
 from .edge import Edge
 
+
 class DirectedEdge(Edge):
     """Represents a directed edge in a graph."""
-    
+
     # Dunder methods
-    
-    def __init__(self, vertices:tuple, weight:int|float=1) -> object:
-        
+
+    def __init__(self, vertices: tuple, weight: int | float = 1) -> object:
+
         from .vertex import Vertex
         assert isinstance(vertices, tuple), "Vertices must be provided in a tuple."
         assert len(vertices) == 2, "The vertex tuple must contain exactly 2 vertex objects."
@@ -22,7 +23,7 @@ class DirectedEdge(Edge):
         vertices[0].add_edge(self)
         vertices[1].add_edge(self)
         self._weight = weight
-    
+
     def __getitem__(self, key) -> object | int | float:
         """In a directed graph, returns the origin [0], destination [1] or weight [2] vertex."""
         assert self._is_directed, "The graph is not directed. use (.vertices) instead."
@@ -32,16 +33,16 @@ class DirectedEdge(Edge):
             return self._weight
         else:
             raise KeyError("The provided key must either be 0, 1 or 2.")
-        
+
     def __del__(self) -> None:
         self._leaves.delete_edge(self)
         self._to.delete_edge(self)
         self._leaves = None
         self._to = None
         self._weight = None
-    
+
     # Properties
-    
+
     @property
     def leaves(self) -> object:
         """Returns the vertex object that the edge is incident from/leaves."""
@@ -51,25 +52,23 @@ class DirectedEdge(Edge):
     def to(self) -> object:
         """Returns the vertex object that the edge is incident to/enters."""
         return self._to
-    
+
     # Instance methods
-    
-    def incident_from(self, vertex:int|object) -> bool:
+
+    def incident_from(self, vertex: int | object) -> bool:
         """Returns True if the edge is incident from/leaves the given vertex."""
         return vertex in self._leaves
-    
-    def incident_to(self, vertex:int|object) -> bool:
+
+    def incident_to(self, vertex: int | object) -> bool:
         """Returns True if the edge is incident to/enters the given vertex."""
         return vertex in self._to
-    
+
     # def remove_vertex(self, vertex):
     #     """Removes the given vertex's references from the edge."""
-    
+
     # Class methods
-    
+
     @classmethod
     def are_parallel(cls, e1, e2) -> bool:
         """Whether the two given directed edges are parallel."""
         return e1.leaves == e2.leaves and e1.to == e2.to
-    
-    
